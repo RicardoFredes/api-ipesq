e55b91cd65af9ab79522dc7c5b8760643e8cb35f
Fredes <ricardofredes@gmail.com>
2020-04-27 19:20:56 -0300

    chore(sequelize): silent log

--- src/db/config.js
+++ src/db/config.js
@@ -2,12 +2,14 @@ module.exports = {
   development: {
     dialect: 'sqlite',
     storage: 'src/db/database.sqlite',
+    logging: false,
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
+    logging: false,
   }
 }
