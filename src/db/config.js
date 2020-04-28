6213bcc6c8a1db3fa4110b55920e59db2db14b4d
Fredes <ricardofredes@gmail.com>
2020-04-28 12:16:54 -0300

    chore: change start script

--- src/db/config.js
+++ src/db/config.js
@@ -1,20 +1,25 @@
+const production = {
+  username: "ybvniujydhmtza",
+  password: "5c90511ee3b73ba939d6120f242d24d850abe80c6124cdedd11e987d43048321",
+  database: "d65gtuupva79a7",
+  host: "ec2-50-17-21-170.compute-1.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+  logging: false,
+}
+
 module.exports = {
-  "development": {
-    "dialect": "sqlite",
-    "storage": "src/db/database.sqlite",
-    "logging": false
+  test: {
+    dialect: "sqlite",
+    storage: "src/db/database.sqlite",
+    logging: false
   },
-  "production": {
-    "username": "ybvniujydhmtza",
-    "password": "5c90511ee3b73ba939d6120f242d24d850abe80c6124cdedd11e987d43048321",
-    "database": "d65gtuupva79a7",
-    "host": "ec2-50-17-21-170.compute-1.amazonaws.com",
-    "port": 5432,
-    "dialect": "postgres",
-    "logging": false,
-    "dialectOptions": {
-      "ssl": true,
-      "sslmode": "require",
+  development: {
+    ...production,
+    logging: true,
+    dialectOptions: {
+      ssl: true,
     }
-  }
+  },
+  production
 }
