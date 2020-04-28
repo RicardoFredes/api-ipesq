3c37f6f72785c7a10e0e1d9238df0076036b8e34
Fredes <ricardofredes@gmail.com>
2020-04-27 21:11:47 -0300

    chore: configure cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 app.use(Router(express.Router))
 app.get('/', (req, res) => res.send('Api Ipesq'))
 
