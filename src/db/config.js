476a873e886d7c3105cd915a1b761d8ebb46aaca
Fredes <ricardofredes@gmail.com>
2020-04-28 12:11:14 -0300

    refactor: credentials

--- src/db/config.js
+++ src/db/config.js
@@ -5,12 +5,16 @@ module.exports = {
     "logging": false
   },
   "production": {
-    "username": "ipesq_amesul",
-    "password": "brasil123",
-    "database": "ipesq1",
-    "host": "db4free.net",
-    "port": 3306,
-    "dialect": "mysql",
-    "logging": false
+    "username": "ybvniujydhmtza",
+    "password": "5c90511ee3b73ba939d6120f242d24d850abe80c6124cdedd11e987d43048321",
+    "database": "d65gtuupva79a7",
+    "host": "ec2-50-17-21-170.compute-1.amazonaws.com",
+    "port": 5432,
+    "dialect": "postgres",
+    "logging": false,
+    "dialectOptions": {
+      "ssl": true,
+      "sslmode": "require",
+    }
   }
 }
